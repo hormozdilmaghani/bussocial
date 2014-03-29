ea0125f09e6710a15d6574b698367ad3ffb28389
Hormoz Dilmaghani <hdilmaghani@gmail.com>
2014-03-29 17:20:31 +0000

    empty environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bussocial::Application.config.secret_key_base = '88c0bf1a849163c57db69c5883747fea789ce542d23eaa3d37b388020194de4d1c9f2034a388dd3d38e7a1016a1ab02fd1049893d38e24be9925d7013ff26db2'
